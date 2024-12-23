5b1a703c4197ec214d2d250b14361f5ee1f99f1e
altcode255 <anonte3p5usu@gmail.com>
2024-12-23 14:45:45 -0800

    Add submission box to site

---
+++ askBox.js
@@ -0,0 +1,47 @@
+const url =
+  "https://script.google.com/macros/s/AKfycbxLA3lB5Mbcc2Kfx0GUCqDbk8ksh9OEDYCrpFssrnPXXmkmALh0JLkBZh8MyqC87A9EFA/exec";
+
+document.getElementById("ask-box").addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const formData = new FormData(this);
+  const data = Object.fromEntries(formData);
+
+  fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "text/plain;charset=utf-8",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log("Successful", data);
+    })
+    .catch((err) => console.log("err", err));
+  var inputs = document.getElementsByClassName("form-control");
+  for (i of inputs) {
+    i.value = "";
+  }
+});
+
+var el;
+
+function countCharacters() {
+  var textEntered, countRemaining, counter;
+  textEntered = this.value;
+  counter = textEntered.length + "/4000";
+  countRemaining = document.getElementById("charactersRemaining");
+  countRemaining.textContent = counter;
+}
+
+function autoGrow() {
+  var fontSize = parseInt(window.getComputedStyle(this).fontSize);
+  this.style.height = fontSize * 7 + "px";
+  this.style.height = this.scrollHeight + "px";
+}
+
+el = document.getElementById("textinput");
+el.addEventListener("keyup", countCharacters, false);
+el.addEventListener("input", autoGrow, false);
+
