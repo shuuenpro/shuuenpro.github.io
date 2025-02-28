a8f41fde0c03cc1033e6fd303e422705045faf23
altcode255 <anonte3p5usu@gmail.com>
2025-02-27 22:32:37 -0800

    don't count hits if local

--- footer.js
+++ footer.js
@@ -10,3 +10,8 @@ function getLastUpdateTimestamp(id, filepath) {
             document.getElementById(id).innerHTML = formatDate + " @ " + formatTime;
         });
 }
+
+function getHits(id, path) {
+    path = path.replace("/", "%2F");
+    document.getElementById(id).src = `https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fshuuenpro.github.io%2F${path}&count_bg=%23FF31B3&title_bg=%23555555&icon_color=%23CB3887&title=Visits&edge_flat=false`
+}
