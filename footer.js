245cb50015fcb70deb5fe868127932bd47d7c03a
altcode255 <anonte3p5usu@gmail.com>
2025-05-17 22:13:14 -0700

    update site urls

--- footer.js
+++ footer.js
@@ -13,5 +13,5 @@ function getLastUpdateTimestamp(id, filepath) {
 
 function getHits(path) {
     path = path.replace("/", "%2F");
-    document.getElementById("hits").src = `https://hitscounter.dev/api/hit?url=https%3A%2F%2Fshuuenpro.github.io${path}&label=Page+Visits&color=%23d63384`;
+    document.getElementById("hits").src = `https://hitscounter.dev/api/hit?url=shuuenpro.github.io${path}&label=Page+Visits&color=%23d63384`;
 }
